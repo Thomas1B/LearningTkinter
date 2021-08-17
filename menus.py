51f71e145fa242bf1691a3624e7328d13e5c2715
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-17 11:31:07 -0700

    multiple menus and sub menus

--- menus.py
+++ menus.py
@@ -16,7 +16,7 @@ root.iconbitmap("Album1/sun_image.ico")
 
 
 def fake_command():
-    pass
+    label = Label(root, text = "You click a menu item!").pack()
 
 
 
@@ -24,7 +24,8 @@ def fake_command():
 
 
 
-# Defing a Menu.
+
+# Defining a Menu.
 my_menu = Menu(root)
 root.config(menu = my_menu)
 
@@ -35,7 +36,26 @@ file_menu.add_command(label = "New", command = fake_command)
 file_menu.add_separator()
 file_menu.add_command(label = "Exit", command = root.quit)
 
+# Creating the second Menu.
+edit_menu = Menu(my_menu)
+my_menu.add_cascade(label = "Edit", menu = edit_menu)
+edit_menu.add_command(label = "Cut", command = fake_command)
+edit_menu.add_command(label = "Copy", command = fake_command)
+edit_menu.add_command(label = "Paste", command = fake_command)
 
+# Creating a third menu
+view_menu = Menu(my_menu)
+my_menu.add_cascade(label = "View", menu = view_menu)
+
+# Creating a sub menu
+options_menu = Menu(my_menu)
+file_menu.add_cascade(label = "Options", menu = options_menu)
+options_menu.add_command(label = "Preferences", command = fake_command)
+options_menu.add_command(label = "Settings", command = fake_command)
+
+
+view_menu.add_command(label = "Increase Font Size", command = fake_command)
+view_menu.add_command(label = "Decrease Font Size", command = fake_command)
 
 
 
