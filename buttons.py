6e6b240be7ab5257577611f21499e9ab35c6a956
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-17 09:35:25 -0700

    intro to Entry Boxes

--- buttons.py
+++ buttons.py
@@ -14,12 +14,19 @@ root.geometry("{}x{}".format(400,400))
 
 
 def click():
-    Label(root, text = "You clicked me!").pack()
+    label1 = Label(root, text = "Hello " + e.get())
+    label1.pack()
 
 
 
+
+Label(root, text = "Enter your name").pack()
+
+e = Entry(root, font = ("Helvetica", 18))
+e.pack(pady=10)
 
 btn1 = Button(root, text = "Click me!", command = click).pack(pady=20)
+
 
 
 
