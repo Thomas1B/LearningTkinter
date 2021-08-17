949a7c53ad20ef58526a04d5d09ae6fc11e3c3da
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-17 09:02:36 -0700

    labels using pack

--- hello.py
+++ hello.py
@@ -11,7 +11,24 @@ root.title("Learning TKinter - hello world")
 root.geometry("{}x{}".format(400,400))
 
 
-label = Label(root, text = "Hello World!").pack()
+label = Label(root, text = "Hello World!")
+label.pack()
+
+'''
+Label options:
+
+fg      - foreground color
+bg      - background color
+height  - height length
+width   - width length
+
+relief - effects. FLAT, RAISED, SUNKEN, GROOVE, RIDGE. default is FLAT.
+.pack(padx=x, pady = y) giving padding to Label.
+
+
+'''
+
+Label(root, text = "Welcome!", fg = "red", bg = "green", font = ("Helvetica", 32), relief = SUNKEN).pack(pady=50)
 
 
 
