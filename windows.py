d60a54e6ff55f921cf91ad76999b058636883a86
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-17 22:04:47 -0700

    Images in new windows

--- windows.py
+++ windows.py
@@ -4,6 +4,7 @@ os.system("clear")
 # ************* Creating New Windows *************
 
 from tkinter import *
+from PIL import ImageTk, Image
 
 
 root = Tk()
@@ -24,18 +25,25 @@ deiconify shows the window.
 def open():
     window = Toplevel()
     window.title("New Window")
-    window.geometry("{}x{}".format(300,200))
+    window.geometry("{}x{}".format(600,800))
     window.iconbitmap("Album1/sun_image.ico")
-    Label(window, text = "This is a new window!").pack(pady=10)
+    Label(window, text = "This is Brisk!", font=("Helvetica, 16")).pack(pady=10)
+
+
+    img = ImageTk.PhotoImage(Image.open("Album1/Brisk1.png"))
+    img_label = Label(window, image=img).pack()
+
+
+
     Button(window, text="Quit", command = window.destroy).pack()
 
     # Minimize Original Window, puts windows to taskbar
-    # Button(window, text="Show Main Window", command = root.deiconify).pack() # show window
-    # Button(window, text="Hide Main Window", command = root.iconify).pack() # hide window
+    Button(window, text="Show Main Window", command = root.deiconify).pack() # show window
+    Button(window, text="Hide Main Window", command = root.iconify).pack() # hide window
 
     # Withdraw Original Window, Doesn't show in taskbar
-    Button(window, text="Show Main Window", command = root.deiconify).pack()
-    Button(window, text="Hide Main Window", command = root.withdraw).pack()
+    # Button(window, text="Show Main Window", command = root.deiconify).pack()
+    # Button(window, text="Hide Main Window", command = root.withdraw).pack()
 
     Button(window, text="Quit Main Window", command = root.destroy).pack()
 
