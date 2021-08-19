dc4789d37879010bf27557ab23a0e4294ecf26e9
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-19 12:04:26 -0700

    added subtraction features, fixed hide_frames function

--- Flashcard_App/flashcard.py
+++ Flashcard_App/flashcard.py
@@ -45,6 +45,33 @@ def add_correct(a, b): # Checking if Addition is correct.
     # Clearing answer box.
     add_answer.delete(0, "end")
 
+
+def subtract_correct(a, b): # Checking if Subtraction is correct.
+    correct = a - b
+
+    # Updating Output Message depending if the given answer is correct.
+    global subtract_answer
+    if int(subtract_answer.get()) == correct:
+        text = "{}-{} = {} is Correct!".format(a,b,subtract_answer.get())
+        subtract_correct_label.config(text=text, font = ("bold", 32))
+    else:
+        text = "{}-{} = {} is Incorrect!{} Try Again!".format(a,b,subtract_answer.get(), "\n\n")
+        subtract_correct_label.config(text=text, font = ("bold", 32))
+
+
+    # Creating a new question if the given answer was correct.
+    if int(subtract_answer.get()) == correct:
+        num1.set(r.randint(0,10))
+        num2.set(r.randint(0,10))
+        text = "{}-{} = ?".format(num1.get(),num2.get())
+        question_label.config(text=text)
+
+    # Clearing answer box.
+    subtract_answer.delete(0, "end")
+
+
+
+
 # Functioning for menu items
 
 def add(): # Addition Function
@@ -71,7 +98,6 @@ def add(): # Addition Function
     add_answer.pack(pady=5)
 
     # Button to submit answer
-    global add_btn
     add_btn = Button(add_frame, text = "Answer!", command = lambda: add_correct(num1.get(), num2.get()))
     add_btn.pack(pady=10)
 
@@ -90,6 +116,39 @@ def subtract(): # Subtraction Function
     hide_frames()
     subtract_frame.pack(fill="both", expand=1)
 
+    # Creating random numbers.
+    global num1
+    global num2
+    num1 = IntVar()
+    num2 = IntVar()
+    num1.set(r.randint(0,10))
+    num2.set(r.randint(0,10))
+
+    # Creating Question label
+    global question_label
+    text = "{}-{} = ?".format(num1.get(),num2.get())
+    question_label = Label(subtract_frame, text=text, font = ('Helvetica', 32))
+    question_label.pack()
+
+    # Creating Entry box
+    global subtract_answer
+    subtract_answer = Entry(subtract_frame)
+    subtract_answer.pack(pady=5)
+
+    # Button to submit answer
+    subtract_btn = Button(subtract_frame, text = "Answer!", command = lambda: subtract_correct(num1.get(), num2.get()))
+    subtract_btn.pack(pady=10)
+
+    # Creating Output Message label, see subtract_correct().
+    global subtract_correct_label
+    text = "Enter the Answer!" # default text
+    subtract_correct_label = Label(subtract_frame, text=text, font = ("bold", 16))
+    subtract_correct_label.pack(pady=5)
+
+
+
+
+
 def multiply(): # Mutliplication Function
     hide_frames()
     multiply_frame.pack(fill="both", expand=1)
@@ -102,6 +161,20 @@ def divide(): # Division Function
 
 
 def hide_frames(): # Hide Function
+
+    for widget in add_frame.winfo_children(): # clearing add_frame of widgets
+        widget.destroy()
+
+    for widget in subtract_frame.winfo_children(): # clearing subtract_frame of widgets
+        widget.destroy()
+
+    for widget in multiply_frame.winfo_children(): # clearing multiply_frame of widgets
+        widget.destroy()
+
+    for widget in divide_frame.winfo_children(): # clearing divide_frame of widgets
+        widget.destroy()
+
+    # Clearing the frames.
     add_frame.pack_forget()
     subtract_frame.pack_forget()
     multiply_frame.pack_forget()
