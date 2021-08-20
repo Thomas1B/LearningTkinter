b0bb46543695373be10d6aebe503d363e5ed02e6
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-19 21:51:29 -0700

    Created a home screen, Added Colors to Program

--- Flashcard_App/flashcard.py
+++ Flashcard_App/flashcard.py
@@ -7,17 +7,23 @@ from tkinter import *
 # from PIL import ImageTk, Image
 import random as r
 
-root = Tk()
-root.title("Learning TKinter - Flashcard")
-root.geometry("{}x{}".format(600,400))
-root.iconbitmap("globe.ico")
+#################################################################
+# ************************** Functions **************************
 
+def start_screen(): # Function for the Screen screen
+    # ************ Start Screen ************
+    hide_frames()
+    start_frame.pack(fill="both", expand=1)
+    text="Welcome to Math Flashcards!"
+    start_label = Label(start_frame, text=text, font=("Helvetica", 20, "bold"), relief=SUNKEN, fg='black', bg='gold', bd=10, padx=10, pady=15)
+    start_label.pack(pady=30)
 
-
-
-# Creating Main Menu
-main_menu = Menu(root)
-root.config(menu = main_menu)
+    # Buttons
+    add_btn = Button(start_frame, text="Addition Flashcards", overrelief=SUNKEN, command=add).pack(ipadx=14,ipady=5)
+    subtract_btn = Button(start_frame, text="Subtraction Flashcards", overrelief=SUNKEN, command=subtract).pack(ipadx=6, ipady=5)
+    multiply_btn = Button(start_frame, text="Mutliplication Flashcards", overrelief=SUNKEN, command=multiply).pack(ipady=5)
+    divide_btn = Button(start_frame, text="Division Flashcards", overrelief=SUNKEN, command=divide).pack(ipadx=16, ipady=5)
+    quit_btn = Button(start_frame, text = "Quit Flashcard", overrelief=SUNKEN, command=root.quit).pack(pady=10, ipadx=28, ipady=5)
 
 
 # ************* Functions to check if the answers given are correct. *************
@@ -30,11 +36,11 @@ def add_correct(a, b): # Checking if Addition is correct.
     global add_answer
     if int(add_answer.get()) == correct:
         text = "{}+{} = {} is Correct!".format(a,b,add_answer.get())
-        add_correct_label.config(text=text, font = ("bold", 32))
+        add_correct_label.config(text=text, font = ("bold", 18))
         check = True
     else:
         text = "{}+{} = {} is Incorrect!{} Try Again!".format(a,b,add_answer.get(), "\n\n")
-        add_correct_label.config(text=text, font = ("bold", 32))
+        add_correct_label.config(text=text, font = ("bold", 18))
 
     # Creating a new question if the given answer was correct.
     if check:
@@ -46,8 +52,6 @@ def add_correct(a, b): # Checking if Addition is correct.
     # Clearing answer box.
     add_answer.delete(0, "end")
 
-
-
 def subtract_correct(a, b): # Checking if Subtraction is correct.
     correct = a - b
     check = False # check for creating a new question
@@ -56,11 +60,11 @@ def subtract_correct(a, b): # Checking if Subtraction is correct.
     global subtract_answer
     if int(subtract_answer.get()) == correct:
         text = "{}-{} = {} is Correct!".format(a,b,subtract_answer.get())
-        subtract_correct_label.config(text=text, font = ("bold", 32))
+        subtract_correct_label.config(text=text, font = ("bold", 18))
         check = True
     else:
         text = "{}-{} = {} is Incorrect!{} Try Again!".format(a,b,subtract_answer.get(), "\n\n")
-        subtract_correct_label.config(text=text, font = ("bold", 32))
+        subtract_correct_label.config(text=text, font = ("bold", 18))
 
     # Creating a new question if the given answer was correct.
     if check:
@@ -72,8 +76,6 @@ def subtract_correct(a, b): # Checking if Subtraction is correct.
     # Clearing answer box.
     subtract_answer.delete(0, "end")
 
-
-
 def multiply_correct(a, b): # Checking if Mutliplication is correct.
     correct = a * b
     check = False # check for creating a new question
@@ -82,11 +84,11 @@ def multiply_correct(a, b): # Checking if Mutliplication is correct.
     global multiply_answer
     if int(multiply_answer.get()) == correct:
         text = "{}x{} = {} is Correct!".format(a, b, multiply_answer.get())
-        multiply_correct_label.config(text=text, font = ("bold", 32))
+        multiply_correct_label.config(text=text, font = ("bold", 18))
         check = True
     else:
         text = "{}x{} = {} is Incorrect!{} Try Again!".format(a, b, multiply_answer.get(), "\n\n")
-        multiply_correct_label.config(text=text, font = ("bold", 32))
+        multiply_correct_label.config(text=text, font = ("bold", 18))
 
     # Creating a new question if the given answer was correct.
     if check:
@@ -98,8 +100,6 @@ def multiply_correct(a, b): # Checking if Mutliplication is correct.
     # Clearing answer box.
     multiply_answer.delete(0, "end")
 
-
-
 def divide_correct(a, b): # Checking if Division is correct.
     correct = round(a / b, 3)
     check = False # check for creating a new question
@@ -108,11 +108,11 @@ def divide_correct(a, b): # Checking if Division is correct.
     global divide_answer
     if float(divide_answer.get()) == correct:
         text = "{}/{} = {} is Correct!".format(a,b,divide_answer.get())
-        divide_correct_label.config(text=text, font = ("bold", 32))
+        divide_correct_label.config(text=text, font = ("bold", 18))
         check = True
     else:
         text = "{}/{} = {} is Incorrect!{} Try Again!".format(a, b, divide_answer.get(), "\n\n")
-        divide_correct_label.config(text=text, font = ("bold", 32))
+        divide_correct_label.config(text=text, font = ("bold", 18))
 
     # Creating a new question if the given answer was correct.
     if check:
@@ -125,8 +125,6 @@ def divide_correct(a, b): # Checking if Division is correct.
     divide_answer.delete(0, "end")
 
 
-
-
 # ************* Functioning for menu items *************
 
 def add(): # Addition Function
@@ -134,8 +132,7 @@ def add(): # Addition Function
     add_frame.pack(fill="both", expand=1)
 
     # Creating random numbers.
-    global num1
-    global num2
+    global num1, num2
     num1 = IntVar()
     num2 = IntVar()
     num1.set(r.randint(0,10))
@@ -144,33 +141,33 @@ def add(): # Addition Function
     # Creating Question label
     global question_label
     text = "{}+{} = ?".format(num1.get(),num2.get())
-    question_label = Label(add_frame, text=text, font = ('Helvetica', 32))
-    question_label.pack()
+    question_label = Label(add_frame, text=text, font = ('Helvetica', 32), bg='light blue')
+    question_label.pack(pady=10)
 
     # Creating Entry box
     global add_answer
     add_answer = Entry(add_frame)
-    add_answer.pack(pady=5)
+    add_answer.pack(pady=5, ipady=5)
 
     # Button to submit answer
-    btn = Button(add_frame, text = "Answer!", command = lambda: add_correct(num1.get(), num2.get()))
+    btn = Button(add_frame, text = "Answer!", font=("bold", 12), overrelief=SUNKEN, padx=10, pady=10, command = lambda: add_correct(num1.get(), num2.get()))
     btn.pack(pady=10)
 
     # Creating Output Message label, see add_correct().
     global add_correct_label
     text = "Enter the Answer!" # default text
-    add_correct_label = Label(add_frame, text=text, font = ("bold", 16))
+    add_correct_label = Label(add_frame, text=text, font = ("bold", 16), bg='light blue')
     add_correct_label.pack(pady=5)
 
-
+    # Button to go back to the home screen.
+    home_btn = Button(add_frame, text="Go to Home Screen", overrelief=SUNKEN, command = start_screen).pack(pady=60, ipadx=5, ipady=5)
 
 def subtract(): # Subtraction Function
     hide_frames()
     subtract_frame.pack(fill="both", expand=1)
 
     # Creating random numbers.
-    global num1
-    global num2
+    global num1, num2
     num1 = IntVar()
     num2 = IntVar()
     num1.set(r.randint(0,10))
@@ -179,33 +176,33 @@ def subtract(): # Subtraction Function
     # Creating Question label
     global question_label
     text = "{}-{} = ?".format(num1.get(),num2.get())
-    question_label = Label(subtract_frame, text=text, font = ('Helvetica', 32))
-    question_label.pack()
+    question_label = Label(subtract_frame, text=text, font = ('Helvetica', 32), bg='light blue')
+    question_label.pack(pady=10)
 
     # Creating Entry box
     global subtract_answer
     subtract_answer = Entry(subtract_frame)
-    subtract_answer.pack(pady=5)
+    subtract_answer.pack(pady=5, ipady=5)
 
     # Button to submit answer
-    btn = Button(subtract_frame, text = "Answer!", command = lambda: subtract_correct(num1.get(), num2.get()))
+    btn = Button(subtract_frame, text = "Answer!", font=("bold", 12), overrelief=SUNKEN, command = lambda: subtract_correct(num1.get(), num2.get()))
     btn.pack(pady=10)
 
     # Creating Output Message label, see subtract_correct().
     global subtract_correct_label
     text = "Enter the Answer!" # default text
-    subtract_correct_label = Label(subtract_frame, text=text, font = ("bold", 16))
+    subtract_correct_label = Label(subtract_frame, text=text, font = ("bold", 16), bg='light blue')
     subtract_correct_label.pack(pady=5)
 
-
+    # Button to go back to the home screen.
+    home_btn = Button(subtract_frame, text="Go to Home Screen", overrelief=SUNKEN, command = start_screen).pack(pady=60, ipadx=5, ipady=5)
 
 def multiply(): # Mutliplication Function
     hide_frames()
     multiply_frame.pack(fill="both", expand=1)
 
     # Creating random numbers.
-    global num1
-    global num2
+    global num1, num2
     num1 = IntVar()
     num2 = IntVar()
     num1.set(r.randint(0,10))
@@ -214,33 +211,33 @@ def multiply(): # Mutliplication Function
     # Creating Question label
     global question_label
     text = "{}x{} = ?".format(num1.get(),num2.get())
-    question_label = Label(multiply_frame, text=text, font = ('Helvetica', 32))
-    question_label.pack()
+    question_label = Label(multiply_frame, text=text, font = ('Helvetica', 32), bg='light blue')
+    question_label.pack(pady=10)
 
     # Creating Entry box
     global multiply_answer
     multiply_answer = Entry(multiply_frame)
-    multiply_answer.pack(pady=5)
+    multiply_answer.pack(pady=5, ipady=5)
 
     # Button to submit answer
-    btn = Button(multiply_frame, text = "Answer!", command = lambda: multiply_correct(num1.get(), num2.get()))
+    btn = Button(multiply_frame, text = "Answer!", font=("bold", 12), overrelief=SUNKEN, command = lambda: multiply_correct(num1.get(), num2.get()))
     btn.pack(pady=10)
 
     # Creating Output Message label, see add_correct().
     global multiply_correct_label
     text = "Enter the Answer!" # default text
-    multiply_correct_label = Label(multiply_frame, text=text, font = ("bold", 16))
+    multiply_correct_label = Label(multiply_frame, text=text, font = ("bold", 16), bg='light blue')
     multiply_correct_label.pack(pady=5)
 
-
+    # Button to go back to the home screen.
+    home_btn = Button(multiply_frame, text="Go to Home Screen", overrelief=SUNKEN, command = start_screen).pack(pady=60, ipadx=5, ipady=5)
 
 def divide(): # Division Function
     hide_frames()
     divide_frame.pack(fill="both", expand=1)
 
     # Creating random numbers.
-    global num1
-    global num2
+    global num1, num2
     num1 = IntVar()
     num2 = IntVar()
     num1.set(r.randint(0,10))
@@ -249,44 +246,41 @@ def divide(): # Division Function
     # Creating Question label
     global question_label
     text = "{}/{} = ?".format(num1.get(),num2.get())
-    question_label = Label(divide_frame, text=text, font = ('Helvetica', 32))
-    question_label.pack()
+    question_label = Label(divide_frame, text=text, font = ('Helvetica', 32), bg='light blue')
+    question_label.pack(pady=10)
+
+    Label(divide_frame, text = "(Round to 3 decimal places if needed)", bg='light blue').pack(pady=5)
 
     # Creating Entry box
     global divide_answer
     divide_answer = Entry(divide_frame)
-    divide_answer.pack(pady=5)
-
-    Label(divide_frame, text = "Round to 3 decimal places if needed").pack()
+    divide_answer.pack(pady=5, ipady=5)
 
     # Button to submit answer
-    btn = Button(divide_frame, text = "Answer!", command = lambda: divide_correct(num1.get(), num2.get()))
+    btn = Button(divide_frame, text = "Answer!", font=("bold", 12), overrelief=SUNKEN, command = lambda: divide_correct(num1.get(), num2.get()))
     btn.pack(pady=10)
 
     # Creating Output Message label, see add_correct().
     global divide_correct_label
     text = "Enter the Answer!" # default text
-    divide_correct_label = Label(divide_frame, text=text, font = ("bold", 16))
+    divide_correct_label = Label(divide_frame, text=text, font = ("bold", 16), bg='light blue')
     divide_correct_label.pack(pady=5)
 
-
+    # Button to go back to the home screen.
+    home_btn = Button(divide_frame, text="Go to Home Screen", overrelief=SUNKEN, command = start_screen).pack(pady=60, ipadx=5, ipady=5)
 
 def hide_frames(): # Hide Function
 
+    # Clearing Frames of widgets
+    for widget in start_frame.winfo_children():
+        widget.destroy()
     for widget in add_frame.winfo_children():
-        # clearing add_frame of widgets
         widget.destroy()
-
     for widget in subtract_frame.winfo_children():
-        # clearing subtract_frame of widgets
         widget.destroy()
-
     for widget in multiply_frame.winfo_children():
-        # clearing multiply_frame of widgets
         widget.destroy()
-
     for widget in divide_frame.winfo_children():
-        # clearing divide_frame of widgets
         widget.destroy()
 
     # Clearing the frames.
@@ -294,14 +288,24 @@ def hide_frames(): # Hide Function
     If a new flashcard is selected (from the menu), this allows the question label to shown
     where the original label was. "Overwrites the label"
     '''
+    start_frame.pack_forget()
     add_frame.pack_forget()
     subtract_frame.pack_forget()
     multiply_frame.pack_forget()
     divide_frame.pack_forget()
 
 
+#################################################################
+# Creating the root Screen.
 
+root = Tk()
+root.title("Learning TKinter - Flashcard")
+root.geometry("{}x{}".format(600,500))
+root.iconbitmap("globe.ico")
 
+# Creating Main Menu
+main_menu = Menu(root)
+root.config(menu = main_menu)
 
 
 # ************* Creating Main Menu items *************
@@ -315,13 +319,13 @@ math_menu.add_separator()
 math_menu.add_command(label="Exit Program", command=root.quit)
 
 
-# Creating math Frames
-add_frame = Frame(root, width=400, height=400)
-subtract_frame = Frame(root, width=400, height=400)
-multiply_frame = Frame(root, width=400, height=400)
-divide_frame = Frame(root, width=400, height=400)
+# ************* Creating Frames *************
+start_frame = Frame(root, width=400, height=400, bg="light blue") # Welcome Screen Frame
+add_frame = Frame(root, width=400, height=400, bg="light blue")
+subtract_frame = Frame(root, width=400, height=400, bg="light blue")
+multiply_frame = Frame(root, width=400, height=400, bg="light blue")
+divide_frame = Frame(root, width=400, height=400, bg="light blue")
 
-
-
+start_screen() # Starting the Welcome Screen.
 
 root.mainloop()
