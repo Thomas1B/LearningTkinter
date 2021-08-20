0782477775178d22af2be13e6da30f3bc4656c3c
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-19 23:21:22 -0700

    inital commit

---
+++ MP3player_App/mp3Player.py
@@ -0,0 +1,20 @@
+# ************** MP3 PLayer App using TKinter module **************
+
+import os
+os.system("clear") # clears terminal
+
+from tkinter import *
+
+
+##################### Main Program #####################
+
+root = Tk()
+root.title("MP3 Player")
+root.geometry("{}x{}".format(500,400))
+root.iconbitmap("music_icon.ico")
+
+
+
+
+
+root.mainloop()
