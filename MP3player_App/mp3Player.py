c7b228e1fcbeec5f10eac19335dff19aa2fbd222
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-21 00:17:01 -0700

    added Play song functions

--- MP3player_App/mp3Player.py
+++ MP3player_App/mp3Player.py
@@ -5,7 +5,10 @@ os.system("clear") # clears terminal
 
 from tkinter import *
 from tkinter import filedialog as fd
+import pygame as pg # for mp3 files
 
+# Initalizing pygame
+pg.mixer.init()
 
 ##################### Functions #####################
 
@@ -18,7 +21,7 @@ def add_song(): # adds a song to the playlist
     playlist_box.insert(END, new_song)
 
 def add_songs(): # adds many songs to the playlist
-    new_songs = fd.askopenfilenames(initialdir="music/", title="MP3 Player - Adding One Song", filetypes=(("Mp3 Files","*.mp3"), ))
+    new_songs = fd.askopenfilenames(initialdir="music/", title="MP3 Player - Adding Many Songs", filetypes=(("Mp3 Files","*.mp3"), ))
 
     # Changing name of new_song to a simplier one. Since askopenfilename return entire FilePath.
     for song in new_songs:
@@ -27,13 +30,17 @@ def add_songs(): # adds many songs to the playlist
         playlist_box.insert(END, song)
 
 def delete_song(): # Function to delete one song from playlist
-    pass
-
-def delete_songs(): # Function to delete many songs from playlist
-    pass
+    playlist_box.delete(ANCHOR)
 
 def delete_all(): # Function to delete all songs from playlist
-    pass
+    playlist_box.delete(0, END)
+
+def play(): # Plays the Song
+    song = playlist_box.get(ACTIVE)
+    song = "C:/Users/thoma/OneDrive/University/PythonStuff/LearningTKinter/MP3player_App/music/{}.mp3".format(song)
+    pg.mixer.music.load(song)
+    pg.mixer.music.play()
+
 
 
 ##################### Main Program #####################
@@ -43,6 +50,7 @@ root = Tk()
 root.title("MP3 Player")
 root.geometry("{}x{}".format(500,400))
 root.iconbitmap("images/music_icon.ico")
+
 
 # Defining Images
 play_img = PhotoImage(file="images/play.png")
@@ -60,7 +68,7 @@ media_frame = Frame(root)
 media_frame.pack(pady=20)
 
 # Creating Media Control Buttons
-play_btn = Button(media_frame, image=play_img, overrelief=SUNKEN, bd=0)
+play_btn = Button(media_frame, image=play_img, overrelief=SUNKEN, bd=0, command=play)
 forward_btn = Button(media_frame, image=forward_img, overrelief=SUNKEN, bd=0)
 back_btn = Button(media_frame, image=back_img, overrelief=SUNKEN, bd=0)
 pause_btn = Button(media_frame, image=pause_img, overrelief=SUNKEN, bd=0)
@@ -89,12 +97,11 @@ add_song_menu.add_command(label="Add Many Songs to the Playlist", command=add_so
 delete_menu = Menu(main_menu, tearoff=0)
 main_menu.add_cascade(label="Delete Songs", menu=delete_menu)
 delete_menu.add_command(label="Delete One Song from the Playlist", command=delete_song)
-delete_menu.add_command(label="Delete Many Songs from the Playlist", command=delete_songs)
 delete_menu.add_command(label="Delete All Songs from the Playlist", command=delete_all)
 
 # Temporary Label
 my_label = Label(root, text="")
-my_label.pack(pady=20)
+my_label.pack(pady=5)
 
 
 
