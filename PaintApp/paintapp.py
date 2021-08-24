6bb355fd3f9f06ca009f752859452d729c9f8c96
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-24 00:49:35 -0700

    initial commit

---
+++ PaintApp/paintapp.py
@@ -0,0 +1,25 @@
+# ************** Paint App using TKinter module **************
+
+import os
+os.system("clear") # clears terminal
+
+from tkinter import *
+
+
+
+##################### Main Program #####################
+
+# Making Main (root) window.
+root = Tk()
+root.title("Paint App")
+root.geometry("{}x{}".format(500,400))
+# root.iconbitmap()
+
+
+
+
+
+
+
+
+root.mainloop()
