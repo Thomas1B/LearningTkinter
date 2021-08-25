8f264c0523a842608ac298e62e34f3cce756d005
Thomas Bourgeois <thomasbourgeois2@hotmail.com>
2021-08-24 20:03:25 -0700

    added Mouse bind to canvas

--- PaintApp/paintapp.py
+++ PaintApp/paintapp.py
@@ -6,6 +6,12 @@ os.system("clear") # clears terminal
 from tkinter import *
 
 
+##################### Functions #####################
+
+def paint(event): # Function to draw
+    canvas.config(bg="black")
+
+
 
 ##################### Main Program #####################
 
@@ -15,6 +21,7 @@ root.title("Paint Program")
 root.geometry("{}x{}".format(800,800))
 # root.iconbitmap()
 
+# Creating Canvas Widgit.
 w = 600 # Width for canvas
 h = 400 # Height for canvas
 canvas = Canvas(root, width=w, height=h, bg="white")
@@ -25,6 +32,7 @@ canvas.pack(pady=20)
 canvas.create_line(0, 100, 300, 100, fill="red")
 canvas.create_line(150, 0, 150, 200, fill="red")
 
+canvas.bind("<B1-Motion>", paint) # left-click on mouse
 
 
 
